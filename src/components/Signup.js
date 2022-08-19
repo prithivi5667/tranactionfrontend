607eca78341383ddc6a48ac93d76b561b104c718
prithivi5667 <prithivi5667@gmail.com>
2022-08-19 16:35:48 +0530

    links

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+//const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Signup = () => {
     let [name, setName] = useState('');
